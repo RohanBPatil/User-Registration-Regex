4ec3ef871653d3c2960cfb12d1c8ddb053edd4f4
RohanBPatil <rohanpatil14218@gmail.com>
2020-09-29 18:07:24 +0530

    Added : Functionality to validate email

--- UserRegistration.java
+++ UserRegistration.java
@@ -11,11 +11,24 @@ public class UserRegistration {
 		System.out.println(firstName + "----->" + patternMatcher.validateName(firstName));
 		String lastName = "Patil";
 		System.out.println(lastName + "----->" + patternMatcher.validateName(lastName));
+		String email = "abc.xyz@bl.co.ukkk";
+		System.out.println(email + "----->" + patternMatcher.validateEmail(email));
+		
 	}
 	
 	
 	private String validateName(String check) {
 		String regex = "^([A-Z]{1}[A-Za-z]{2,})$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(check);
+		if(matcher.find()) {
+			return "valid";
+		}
+		return "invalid";
+	}
+	
+	private String validateEmail(String check) {
+		String regex = "^(abc[.][A-Za-z]+@bl[.]co[.][A-Za-z]{2,})$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(check);
 		if(matcher.find()) {
